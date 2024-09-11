f2c330bf0abd03fa2fb63f61a1a2cdf124610b0d
Tomer Abady <tomer@Tomers-MacBook-Pro-2.local>
2024-09-11 10:46:56 +0300

    intro completed

--- pages/Components/Common/Select/CustomSelect.tsx
+++ pages/Components/Common/Select/CustomSelect.tsx
@@ -4,11 +4,20 @@ import styles from "./CustomSelect.module.scss";
 interface Props extends SelectProps {}
 
 const CustomSelect: React.FC<Props> = (props) => {
-  const { selectProps } = styles;
+  const { selectProps, selectLabel } = styles;
+  type LabelRender = SelectProps["labelRender"];
+
+  const labelRender: LabelRender = (props) => {
+    const { label, value } = props;
+    return <span className={selectLabel}>{label}</span>;
+  };
   return (
     <Select
       {...props}
       className={selectProps}
+      // variant="outlined"
+      labelRender={labelRender}
+      // popupClassName={popupProps}
       optionRender={(option) => (
         <Space>
           <span role="img">{option.data.emoji}</span>
--- pages/Components/Intro/intro.tsx
+++ pages/Components/Intro/intro.tsx
@@ -10,8 +10,10 @@ const Intro: React.FC = () => {
     coverContent,
     coverContentItem,
     coverContainer,
-    introText,
+
     titleText,
+    coverGradient,
+    subtitleText,
     bodyText,
     doctorImage,
   } = styles;
@@ -21,37 +23,20 @@ const Intro: React.FC = () => {
     <div className={introContainer}>
       <div className={coverContainer}>
         <CustomImage
-          src={"/assets/images/cover3.jpg"}
+          src={"/assets/images/cover5.jpg"}
           width={0}
           height={0}
           style={{
-            objectPosition: isMobile ? "top" : "50% 70%",
+            objectPosition: isMobile ? "35%" : "50%",
             objectFit: "cover",
             height: "100%",
             width: "100%",
           }}
         />
-        <div className={coverContent}>
-          <div className={coverContentItem}>
-            <span className={cn(introText, titleText)}>{t("intro.a")}</span>
-            <span className={cn(introText, bodyText)}>{t("intro.b")}</span>
-            <span className={cn(introText, bodyText)}>{t("intro.c")}</span>
-            <span className={cn(introText, bodyText)}>{t("intro.d")}</span>
-            {/* <span className={cn(introText, introBody)}>{t("intro.e")}</span> */}
-          </div>
-          <div className={coverContentItem}>
-            <CustomImage
-              src={"/assets/images/doctor.png"}
-              className={doctorImage}
-              height={isMobile ? 200 : 400}
-              width={0}
-              sizes=""
-              style={{
-                objectPosition: "left",
-                objectFit: "contain",
-                width: "auto",
-              }}
-            />
+        <div className={coverGradient}>
+          <div className={coverContent}>
+            <h1 className={titleText}>{t("intro.name")}</h1>
+            <span className={subtitleText}>{t("intro.a")}</span>
           </div>
         </div>
       </div>
@@ -60,3 +45,13 @@ const Intro: React.FC = () => {
 };
 
 export default Intro;
+
+/*
+          <div className={coverContentItem}>
+            <span className={cn(introText, titleText)}>{t("intro.a")}</span>
+            <span className={cn(introText, bodyText)}>{t("intro.b")}</span>
+            <span className={cn(introText, bodyText)}>{t("intro.c")}</span>
+            <span className={cn(introText, bodyText)}>{t("intro.d")}</span>
+            {/* <span className={cn(introText, introBody)}>{t("intro.e")}</span> 
+            </div>
+*/
