80c4906a50ce8694172afa87a29e9202c7fa0e73
Tomer Abady <tomer@Tomers-MacBook-Pro-2.local>
2024-10-06 15:47:11 +0300

    added icon and title, performed cleanup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Dr. Skliarok</title>
       <Head />
+      <link rel="icon" href="/assets/images/logo.png" />
       <body style={{ margin: 0 }}>
         <Main />
         <NextScript />
--- public/assets/icons/iconLib.tsx
+++
@@ -1,5 +0,0 @@
-import ToothIcon from "./tooth";
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  ToothIcon,
-};
